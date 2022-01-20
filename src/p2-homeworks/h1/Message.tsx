43f41051f4cae1a7d4f8c51fafe5e99c486f3be3
EugeneMNS <evgen.str4@gmail.com>
2022-01-20 13:26:37 +0300

    HW 1 unfinished

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,25 @@
+import { type } from 'os'
 import React from 'react'
-
-function Message() {
+import  s  from './Message.module.css'
+type MessagePropseType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+function Message(props: MessagePropseType) {
     return (
-        <div>
-
+        <div className= {s.message}>
+            <img className={s.avatar} src={props.avatar} alt="avatar"/>
+            <div className={s.message__angle}/>
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
+
 
 export default Message
